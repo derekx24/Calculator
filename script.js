9d9cfa6c0b4cbcf60e19deb8569f60d353f42a6c
Derek Xu <derek24@students.cs.ubc.ca>
2023-03-26 23:36:23 -0700

    add Ans, AC, and Del buttons

--- script.js
+++ script.js
@@ -6,10 +6,17 @@ let result = '';
 
 const display = document.getElementById('display');
 const previous = document.getElementById('previous');
+const ansButton = document.querySelector('.ans-btn');
+const acButton = document.querySelector('.ac-btn');
+const delButton = document.querySelector('.del-btn');
 const numberButton = document.querySelectorAll('.n-btn');
 const operatorButton = document.querySelectorAll('.o-btn');
 const decimalButton = document.querySelector('.d-btn');
 const equalsButton = document.querySelector('.e-btn');
+
+ansButton.onclick = () => inputPreviousAns();
+acButton.onclick = () => clearScreen();
+delButton.onclick = () => deletePrevious();
 
 numberButton.forEach((button) => {
     button.addEventListener('click', () => inputNumber(button.textContent));
@@ -25,6 +32,37 @@ window.addEventListener('keydown', handleKey);
 
 function updateDisplay() {
     display.textContent = firstNum + operator + secondNum;
+}
+
+function inputPreviousAns() {
+    if (result === '') return;
+    if (equationState === 1) {
+        firstNum += result;
+    } else {
+        equationState = 3;
+        secondNum += result;
+    }
+    updateDisplay();
+}
+
+function clearScreen() {
+    reset();
+    if (result != '') previous.textContent = `Ans = ${result}`;
+    updateDisplay();
+    display.textContent = '0';
+}
+
+function deletePrevious() {
+    if (result != '') previous.textContent = `Ans = ${result}`;
+    if (equationState == 1) {
+        firstNum = firstNum.toString().slice(0, -1);
+    } else if (equationState === 2 || ((equationState === 3) & (secondNum === ''))) {
+        operator = '';
+        equationState = 1;
+    } else {
+        secondNum = secondNum.toString().slice(0, -1);
+    }
+    updateDisplay();
 }
 
 function inputNumber(number) {
@@ -43,6 +81,7 @@ function inputNumber(number) {
 }
 
 function selectOperator(o) {
+    if (equationState === 3) return;
     if (result != '') previous.textContent = `Ans = ${result}`;
     if (firstNum === '' && result != '') {
         firstNum = result;
@@ -55,7 +94,7 @@ function selectOperator(o) {
 
 function addDecimal() {
     if (equationState === 1) {
-        if (firstNum === 0) firstNum = '0.';
+        if (firstNum === '') firstNum = '0.';
         if (!firstNum.includes('.')) firstNum += '.';
     } else if (equationState === 2) {
         equationState = 3;
@@ -97,7 +136,7 @@ function reset() {
 }
 
 function performOperation() {
-    if (secondNum == '') return;
+    if (secondNum === '') return;
     result = round(calculate());
     previous.textContent = firstNum + operator + secondNum + '=';
     display.textContent = result;
@@ -105,6 +144,8 @@ function performOperation() {
 }
 
 function handleKey(e) {
+    if (e.key === 'a') inputPreviousAns();
+    if (e.key === 'Backspace') deletePrevious();
     if (e.key >= 0 && e.key <= 9) inputNumber(e.key);
     if (e.key === '.') addDecimal();
     if (e.key === '=' || e.key === 'Enter') performOperation();
