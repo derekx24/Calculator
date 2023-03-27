59d8309fd0ff27f8c5405ed0c361bd770140b192
Derek Xu <derek24@students.cs.ubc.ca>
2023-03-26 21:09:19 -0700

    implement calculate

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ let equationState = 1;
 let firstNum = "";
 let operator = "";
 let secondNum = "";
+let result = "";
 
 const display = document.getElementById('display');
 const numberButton = document.querySelectorAll('.n-btn');
 const operatorButton = document.querySelectorAll('.o-btn');
 const decimalButton = document.querySelector('.d-btn');
+const equalsButton = document.querySelector('.e-btn');
 
 numberButton.forEach((button) => {
     button.addEventListener('click', () => inputNumber(button.textContent));
@@ -18,6 +20,7 @@ operatorButton.forEach((button) => {
 })
 
 decimalButton.onclick = () => addDecimal();
+equalsButton.onclick = () => performOperation();
 
 function updateDisplay() {
     display.textContent = firstNum + operator + secondNum;
@@ -47,12 +50,47 @@ function addDecimal() {
     if (equationState === 1) {
         if (firstNum === 0) firstNum = '0.';
         if (!firstNum.includes('.')) firstNum += '.';
-    } else if (equationState === 3) {
-        if (secondNum === '') secondNum = '0.';
-        if (!secondNum.includes('.')) secondNum += '.';
-    } else {
+    } else if (equationState === 2) {
         equationState = 3;
         secondNum = '0.';
+    } else {
+        if (secondNum === '') secondNum = '0.';
+        if (!secondNum.includes('.')) secondNum += '.';
     }
     updateDisplay();
+}
+
+function calculate() {
+    let a = Number(firstNum);
+    let b = Number(secondNum);
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '−':
+          return a - b;
+        case '×':
+          return a * b;
+        case '÷':
+          if (secondNum === 0) return 'error';
+          return a / b;
+        default:
+          return firstNum;
+      }
+}
+
+function round(n) {
+    return (Math.round(n * 1000000) / 1000000);
+}
+
+function reset() {
+    equationState = 1;
+    firstNum = "";
+    operator = "";
+    secondNum = "";
+}
+
+function performOperation() {
+    result = round(calculate());
+    display.textContent = result;
+    reset();
 }
